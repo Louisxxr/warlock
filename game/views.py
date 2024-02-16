29235a6ca6b91740a749df98a4d192eb08ffe21f
Louis <1846524782@qq.com>
2024-02-16 20:23:44 +0800

    hello world

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">WARLOCK</h1>'
+    line2 = '<img src="https://c-ssl.duitang.com/uploads/item/201408/18/20140818131405_FFSPC.jpeg">'
+    return HttpResponse(line1 + line2)
+
